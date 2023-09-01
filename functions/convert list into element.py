80fd5ec2d815048636c97713920e0143140d8322
Kavinirtt <113616465+Kavinirtt@users.noreply.github.com>
2023-09-01 19:03:20 +0530

    Delete functions directory

--- functions/convert list into element.py
+++
@@ -1,8 +0,0 @@
-def convert(a):
-    var=""
-    for i in a:
-        var=var+str(i)
-    return int(var)
-a=[1,2,3,4]
-var=convert(a)
-print(var)
--- functions/rectangle.py
+++
@@ -1,8 +0,0 @@
-def Area_Rectangle(l,b):
-    c=l*b
-    return c
-    
-user_input1=int(input("enter the length : "))
-user_input2=int(input("enter the breath : "))
-c=Area_Rectangle(user_input1,user_input2)
-print(c)
